933bfb86f713f0b2ee5d8a2959c971ee7a30f2ba
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-07-14 19:54:49 +0300

    Adjust banner content

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 function updateDateTime() {
     const now = new Date();
     const day = String(now.getDate()).padStart(2, '0');
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // Monate sind nullbasiert
+    const month = String(now.getMonth() + 1).padStart(2, '0');
     const year = now.getFullYear();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
