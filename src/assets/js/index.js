024636b5bcc051988ef717bce0d1b9e1dba98e9d
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-07-14 19:40:49 +0300

    Add updateDateTime() function

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,17 @@
 import "../styles/app.scss";
+
+document.addEventListener("DOMContentLoaded", function () {
+    updateDateTime();
+    setInterval(updateDateTime, 60000);
+});
+
+function updateDateTime() {
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Monate sind nullbasiert
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const formattedDateTime = `${day}.${month}.${year} | ${hours}:${minutes}`;
+    document.getElementById('datetime').textContent = formattedDateTime;
+}
